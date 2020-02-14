9a182f469fc64710c21da9ba2a799eee888dc08e
erda1010 <2305erda@gmail.com>
2020-02-14 16:35:59 +0100

    Validation form input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,37 @@
-import React, { Component } from "react";
+import React, { useCallback } from "react";
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/components/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>New place</h2>;
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid text"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="description"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description"
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
